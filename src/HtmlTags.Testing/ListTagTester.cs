e055a14ff6786d4dc8c807c1a3e8414e5e3a1424
matt.clevenger <matt.clevenger@gmail.com>
2010-07-30 15:38:45 -0600

    Added ListTag and tests for same

---
+++ src/HtmlTags.Testing/ListTagTester.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace HtmlTags.Testing
+{
+    [TestFixture]
+    public class ListTagTester
+    {
+        private ListTag _listTag;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _listTag = new ListTag();
+        }
+
+        [Test]
+        public void should_have_ul_as_top_level_element()
+        {
+            _listTag.TagName().ShouldEqual("ul");
+        }
+
+        [Test]
+        public void should_have_li_child_element_when_add_item_text()
+        {
+            var text = "an item";
+            _listTag.AddItem(text);
+
+            _listTag.Children.ShouldHaveCount(1);
+            var item = _listTag.Children.First();
+            item.TagName().ShouldEqual("li");
+            item.Text().ShouldEqual(text);
+        }
+
+        [Test]
+        public void should_have_li_child_element_when_add_item_child()
+        {
+            var child = new LinkTag("foo", "url");
+            _listTag.AddItem(child);
+
+            _listTag.Children.ShouldHaveCount(1);
+            var item = _listTag.Children.First();
+            item.TagName().ShouldEqual("li");
+            item.Children.ShouldHaveCount(1);
+            item.Children.First().ShouldEqual(child);
+        }
+
+        [Test]
+        public void should_configure_child()
+        {
+            var text = "text";
+            _listTag.AddItem(li => li.Text(text));
+
+            _listTag.Children.ShouldHaveCount(1);
+            var item = _listTag.Children.First();
+            item.Text().ShouldEqual(text);
+        }
+
+    }
+}
---
+++ src/HtmlTags/ListTag.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HtmlTags
+{
+    public class ListTag : HtmlTag
+    {
+
+        public ListTag() : base("ul")
+        {
+            
+        }
+
+        public ListItemTag AddItem(string itemText)
+        {
+            var item = Child<ListItemTag>();
+            item.Text(itemText);
+            return item;
+        }
+
+        public ListItemTag AddItem(HtmlTag itemTag)
+        {
+            var item = Child<ListItemTag>();
+            item.Child(itemTag);
+            return item;
+        }
+
+        public ListItemTag AddItem(Action<ListItemTag> configure)
+        {
+            var item = Child<ListItemTag>();
+            configure(item);
+            return item;
+        }
+
+    }
+
+    public class ListItemTag : HtmlTag
+    {
+        public ListItemTag() : base("li"){}
+    }
+}
